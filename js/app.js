c3f250bb9bc05675435af5a33c90ff1745cacba6
Pooja <malvi.pooja259@gmail.com>
2016-11-07 00:16:00 +0530

    Added Sliders

--- js/app.js
+++ js/app.js
@@ -6,5 +6,42 @@ $(document).ready(function() {
         $('.time-part').html(momentNow.format('hh:mm:ss'));
     }, 100);
     $('.carousel').carousel();
-});
+    $('.video-slider').carousel({
+        interval: false
+    });
+    $('.carousel-item').carousel({
+        interval: false
+    });
+    $('.video-slider .item').each(function() {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
 
+        for (var i = 0; i < 4; i++) {
+            next = next.next();
+            if (!next.length) {
+                next = $(this).siblings(':first');
+            }
+
+            next.children(':first-child').clone().appendTo($(this));
+        }
+    });
+    $('.carousel-item .item').each(function() {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        for (var i = 0; i < 3   ; i++) {
+            next = next.next();
+            if (!next.length) {
+                next = $(this).siblings(':first');
+            }
+
+            next.children(':first-child').clone().appendTo($(this));
+        }
+    });
+});
